2577f17f81f0c4cfca251d67ac73c95ceaead869
nayutatata <1943157602@qq.com>
2026-03-23 13:53:56 +0800

    feat: impl for entire tokenizer

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,5 +1,5 @@
 import os
-from typing import BinaryIO
+from typing import BinaryIO, Iterable, Iterator
 from .pretokenization_example import find_chunk_boundaries
 import regex as re
 from collections import Counter
@@ -7,6 +7,7 @@ import multiprocessing as mp
 import pickle
 import cProfile
 import pstats
+import json
 '''
 1. 先把文本划分成一些pre token。统计pre token的出现频率
 2. 统计pre token内部byte pair的出现频率
@@ -108,25 +109,84 @@ def TrainBpe(
     return vocab, merges
 
 class MyTokenizer:
-    def __init__(self, vocab:dict[int,bytes], merges:list[tuple[bytes,bytes]]):
+    def __init__(self, vocab:dict[int,bytes], merges:list[tuple[bytes,bytes]], specialTokens:list[str]=None):
         self.vocab = vocab
         self.vocab_inverse = {v:u for u,v in vocab.items()}
-        
+        self.specialTokens = sorted(specialTokens, key=lambda s: len(s), reverse=True) if specialTokens else []
+        self.merges = {tup: self.vocab_inverse[tup[0]+tup[1]] for tup in merges}
+    
+    @classmethod
+    def from_files(cls, vocab_path:str, merges_path:str, special_tokens:list[str]):
+        with open(vocab_path, 'r') as f:
+            data = json.load(f)
+            vocab = {v:u.encode() for u,v in data.items()}
+        with open(merges_path, 'r') as f:
+            lines = f.readlines()
+            merges = []
+            for line in lines:
+                vec = line.strip().split(' ')
+                merges.append((vec[0].encode(), vec[1].encode()))
+        return cls(vocab, merges, special_tokens)
+    
+    def PreTokenize(self, text:str)->list[bytes]:
+        # 按照speical_tones进行分割
+        if self.specialTokens:
+            escapeToks = [re.escape(tok) for tok in self.specialTokens]
+            pattern = f'({"|".join(escapeToks)})'
+            segs = re.split(pattern, text)
+        else:
+            segs = [text]
+        preTokens = []
+        for seg in segs:
+            if seg == "":
+                continue
+            if self.specialTokens and  seg in self.specialTokens:
+                preTokens.append(seg.encode())
+                continue
+            #pre tokenize
+            PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+            matches = re.finditer(PAT, seg)
+            for m in matches:
+                text = m.group().encode('utf-8')
+                preTokens.append(text)
+        return preTokens
+    
+    def encode(self, text: str) -> list[int]:
+        preTokens = self.PreTokenize(text)
+        preTokensSplit = [list(bytes([i]) for i in token) for token in preTokens]
+        # print(f'preTokens={preTokens}')
+        ids = []
+        for lst, s in zip(preTokensSplit, preTokens):
+            if self.specialTokens and  s.decode() in self.specialTokens:
+                print(f'{s} is a special token')
+                ids.append(self.vocab_inverse[s])
+                continue
+            while len(lst)>1:
+                pairs = [(lst[i], lst[i+1]) for i in range(len(lst)-1)]
+                pairIds = [self.vocab_inverse.get(pair[0]+pair[1], None) for pair in pairs]
+                if all([pid is None for pid in pairIds]):
+                    break
+                bestPair = min([(pair, pid) for pair, pid in zip(pairs, pairIds) if pid is not None], key=lambda i: i[1])
+                i=0
+                newList = []
+                while i<len(lst):
+                    if i<len(lst)-1 and (lst[i], lst[i+1])==bestPair[0]:
+                        newList.append(self.vocab[bestPair[1]])
+                        i+=2
+                    else:
+                        newList.append(lst[i])
+                        i+=1
+                lst = newList
+            ids.extend([self.vocab_inverse[token] for token in lst])
+        return ids
 
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+        for text in iterable:
+            yield from self.encode(text)
+
+    def decode(self, ids: list[int]) -> str:
+        return b''.join(self.vocab[i] for i in ids).decode(errors='replace')
 
         
 if __name__ == '__main__':
-    profiler = cProfile.Profile()
-    profiler.enable()
-    v, m = TrainBpe('/home/ljx123/cs336/assignment1-basics/data/TinyStories-valid.txt', 1000, ["<|endoftext|>"])
-    profiler.disable()
-    with open('profile-log/train_bpe.txt', 'w') as f:
-        stats = pstats.Stats(profiler, stream=f).sort_stats('cumulative')
-        stats.print_stats()
-
-    # weightDir =  '/home/ljx123/cs336/assignment1-basics/tokenizer-data'
-    # print(m)
-    # with open(os.path.join(weightDir, 'vocab.pkl'), 'wb') as f:
-    #     pickle.dump(v, f)
-    # with open(os.path.join(weightDir, 'merges.pkl'), 'wb') as f:
-    #     pickle.dump(m, f)
+    pass
--- tests/adapters.py
+++ tests/adapters.py
@@ -8,7 +8,7 @@ import numpy.typing as npt
 import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
-from cs336_basics.tokenizer import TrainBpe
+from cs336_basics.tokenizer import TrainBpe, MyTokenizer
 
 
 def run_linear(
@@ -560,7 +560,7 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+    return MyTokenizer(vocab, merges, special_tokens)
 
 
 def run_train_bpe(
